41042eef0bb039b8755c0bdc173e0f1c2e67233c
Vanitha.k <kavuruvanitha@gmail.com>
2025-12-23 21:48:35 +0530

    after setup with expo go

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hai</Text>
+      <Text>Hai vanitha</Text>
      
     </View>
   );
